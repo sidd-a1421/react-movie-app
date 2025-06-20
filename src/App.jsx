72b1dc2efeb3c8c96ce0d7ba91a85783362f419f
Achinta <achintasiddhanta0304@gmail.com>
2025-06-21 00:01:54 +0530

    update the public folder and html document name

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <main>
+      <div className='pattern'>
+        <div className='wrapper'>
+          <header>
+            <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+          </header>
+          <p>Search</p>
+        </div>
+      </div>
+    </main>
   )
 }
 
