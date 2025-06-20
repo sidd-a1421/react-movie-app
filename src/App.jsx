26c93617011ce5dfab1e0c0969b2a9781b5ce86d
Achinta <achintasiddhanta0304@gmail.com>
2025-06-21 01:05:18 +0530

    modify app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { useState } from 'react';
+import Search from './components/Search';
 
 const App = () => {
 
@@ -9,9 +10,10 @@ const App = () => {
       <div className='pattern'>
         <div className='wrapper'>
           <header>
+            <img src="./hero.png" alt="Hero Banner" />
             <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
           </header>
-          <p>Search</p>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </div>
       </div>
     </main>
